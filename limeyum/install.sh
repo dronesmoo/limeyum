ea5460e38cbd823dc1afe96669318dc50c73bcf0
dronesmoo <83227574+dronesmoo@users.noreply.github.com>
2026-08-07 12:33:47 -0500

    Update install.sh

--- limeyum/install.sh
+++ limeyum/install.sh
@@ -32,6 +32,8 @@ cp limeyum "$THISPATH/"
 cp -r modules/* "$THISPATH/modules/" 2>/dev/null
 cp -r lib/* "$THISPATH/lib/" 2>/dev/null
 #cp -r templates/* "$THISPATH/templates/" 2>/dev/null
+touch "$THISPATH/lime.conf"
+echo "HOME="/lime" >> "$THISPATH/lime.conf"
 echo "[+] Updating file permissions"
 chmod +x ./modules/*
 chmod +x limeyum
